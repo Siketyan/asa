a03dbb41361e7e1a85bbd4ebff864d57b6d4a0dd
Naoki Ikeguchi <root@siketyan.dev>
2020-03-01 17:50:41 +0900

    🗃 Add database context

---
+++ src/Asa/DbContexts/AppDbContext.cs
@@ -0,0 +1,18 @@
+using Asa.Models;
+using Asa.Models.Activity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asa.DbContexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Directory> Directories { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<Line> Lines { get; set; }
+        
+        public DbSet<Insertion> Insertions { get; set; }
+        public DbSet<Modification> Modifications { get; set; }
+        public DbSet<Deletion> Deletions { get; set; }
+    }
+}
